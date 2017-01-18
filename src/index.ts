b90f9c437c9e116150c92f28212bf4d78cf20f82
Piotr Woloszun <piotr.woloszun@gmail.com>
2017-01-18 04:48:08 +0100

    v1.0.0

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { sum } from './sum';
+
+let result: number = sum(2, 3);
+
+console.log("Result", result);
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const config = {
+  // create entry point
+  //TODO
+
+  // define output file(s)
+  //TODO
+
+  // define module loaders which preprocess src files
+  // handle .ts, .css, .sass, images etc.
+  // loaders enhance webpack, are separate libs
+  //TODO
+
+  // only discover files that have those extensions
+  //TODO
+};
+
+module.exports = config;
